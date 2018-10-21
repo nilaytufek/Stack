c2b07ff8f980681559df00e4537b2a6acb7d2374
nilaytufek <ntufek@gmail.com>
2018-10-21 23:45:46 +0100

    Add files via upload

---
+++ Stack/MyStack.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack
+{
+    public class Node<V>
+    {
+        public V value;
+
+        public Node(V v)
+        {
+            value = v;
+        }
+    }
+
+    class MyStack<V>
+    {
+        Node<V>[] stack = new Node<V>[0];
+        int count = 0;
+        int size = 0;
+        public MyStack(int _size)
+        {
+            size = _size;
+            Array.Resize(ref stack, size);
+        }
+
+        public void Push(Node<V> node)
+        {
+            if (IsFull() == false)
+            {
+                stack[count] = node;
+                count++;
+            }
+        }
+        public V[] GetStack()
+        {
+            V[] arr = new V[count];
+
+            for (int i = count; i > 0; i--)
+            {
+                arr[count-i] = stack[i-1].value;
+            }            
+            return arr;
+        }
+        public V Pop()
+        {
+            if (IsEmpty() == false)
+            {
+                count--;
+                return stack[count].value;
+            }
+            return default(V);
+        }
+
+        public bool Search(V val)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (0 == Comparer<V>.Default.Compare(stack[i].value, val))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        private bool IsFull()
+        {
+            if (count == size - 1)
+            {
+                return true;
+            }
+            return false;
+        }
+        private bool IsEmpty()
+        {
+            if (count == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
---
+++ Stack/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack<int> myStack = new MyStack<int>(20);
+            Node<int> e1 = new Node<int>(1);
+            myStack.Push(e1);
+            Node<int> e2 = new Node<int>(2);
+            myStack.Push(e2);
+            Node<int> e3 = new Node<int>(3);
+            myStack.Push(e3);
+            //print stack
+            Console.WriteLine("myStack: " + Environment.NewLine + string.Join(Environment.NewLine, myStack.GetStack()));
+            //pop
+            int retVal = myStack.Pop();
+            Console.WriteLine("Popped value: " + retVal);
+            //print stack again
+            Console.WriteLine("myStack: " + Environment.NewLine + string.Join(Environment.NewLine, myStack.GetStack()));
+
+            //search - true
+            Console.WriteLine(myStack.Search(e1.value));
+            //search - false
+            Console.WriteLine(myStack.Search(e3.value));
+
+            Console.ReadLine();
+        }
+    }
+}
